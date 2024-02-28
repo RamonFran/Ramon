9ad3bfa25a24347b4087f1a951320a0ef7ed1c9f
RamonFran <franco.ramon@escola.pr.gov.br>
2024-02-28 07:53:53 -0300

    v1

---
+++ Back-End/at1.js
@@ -0,0 +1,7 @@
+let valorA = 20;
+let valorB = 10;
+
+console.log('Soma ' + (valorA + valorB));
+console.log('Divisão ' + (valorA / valorB));
+console.log('Multiplicação ' + (valorA * valorB));
+console.log('Subtração ' + (valorA - valorB));
---
+++ Des-Sis/calculadora.py
@@ -0,0 +1,22 @@
+print("Digite 2 valores")
+
+val_1 = float(input("Digite o 1º valor "))
+val_2 = float    (input("Digite o 2º valor "))
+
+# print("A soma é", val_1 + val_2)
+# print("A subtração é", val_1 - val_2)
+# print("A divisão é", val_1 / val_2)
+# print("A multiplicação é", val_1 * val_2)
+
+operacao = int(input("Digite a operação desejada: 1 = Adição, 2 = Subtração, 3 = Multiplicação, 4 = Divisão "))
+
+if operacao == 1:
+    print("A soma é ", val_1 + val_2)
+elif operacao == 2:
+    print("A subtração é ", val_1 - val_2)
+elif operacao == 3:
+    print("A multiplicação é ", val_1 * val_2)
+elif operacao == 4:
+    print("A divisão é ", val_1 / val_2)
+else :
+    print("Siga as instruções")
